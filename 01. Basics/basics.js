019c3f4d21dad3e83d1a5b87ce388c2e2dfb32d0
Mark Jesper Pillerva <mark123jesper@yahoo.com>
2021-08-09 08:46:52 +0800

    Done Folder 1

--- 01. Basics/basics.js
+++ 01. Basics/basics.js
@@ -1,1 +1,6 @@
+// External javascript
+
 console.log("hello world")
+document.write("test");
+document.getElementById("test").innerHTML = "Inserted Text using Inner HTML";
+document.getElementById("test2").innerText = "Inserted Text using Inner Text";
